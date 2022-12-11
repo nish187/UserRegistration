9acac42a850bfe6b0eeeeed7962cb8f31fdbfeed
nisha <nishantsingh187@gmail.com>
2022-12-12 01:33:41 +0530

    Valid Email Address

--- src/UserRegistrationMain.java
+++ src/UserRegistrationMain.java
@@ -14,6 +14,7 @@ public class UserRegistrationMain {
             System.out.println("Enter 0 : Exit");
             System.out.println("Enter 1 : To validate First Name ");
             System.out.println("Enter 2 : To validate Last Name ");
+            System.out.println("Enter 3 : To validate E-mail ");
 
             switch (scanner.nextInt()) {
                 case 0:
@@ -26,6 +27,8 @@ public class UserRegistrationMain {
                 case 2:
                     UserRegistration.validLastName();
                     break;
+                case 3:
+                    UserRegistration.validEmail();
 
             }
         }
