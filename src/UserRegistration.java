fbed680c9927f981ac29e7bd559b093f2bb77a56
nisha <nishantsingh187@gmail.com>
2022-12-12 01:24:49 +0530

    Enter Valid Last Name

--- src/UserRegistration.java
+++ src/UserRegistration.java
@@ -4,31 +4,41 @@ import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-    public class UserRegistration {
-        static final Scanner scanner = new Scanner(System.in);
+public class UserRegistration {
+    static final Scanner scanner = new Scanner(System.in);
 
-        /*
-         * @Purpose : Take First Name And Check It Is Getting Matched With Regex/Regular
-         * Expression Or Not
-         *
-         * @param : Name, Regex, Matches
-         */
 
-        public static void validFirstName() {
 
-            System.out.println("Enter First Name :");
-            String name = scanner.next();
-            String regex = "^[A-Z]{1}[a-z]{2,}$";
+    public static void validFirstName() {
 
-            Pattern p = Pattern.compile(regex);
-            Matcher m = p.matcher(name);
-            boolean r = m.matches();
+        System.out.println("Enter First Name :");
+        String name = scanner.next();
+        String regex = "^[A-Z]{1}[a-z]{2,}$";
 
-            if (r)
-                System.out.println("It Is A Valid First Name");
-            else
-                System.out.println("It Is Invalid First Name");
+        Pattern p = Pattern.compile(regex);
+        Matcher m = p.matcher(name);
+        boolean r = m.matches();
 
-        }
+        if (r)
+            System.out.println("It Is A Valid First Name");
+        else
+            System.out.println("It Is Invalid First Name");
+
     }
+    public static void validLastName() {
 
+        System.out.println("Enter Last Name:");
+        String lname = scanner.next();
+        String regex = "^[A-Z]{1}[a-z]{2,}$";
+
+        Pattern p = Pattern.compile(regex);
+        Matcher m = p.matcher(lname);
+        boolean r = m.matches();
+
+        if (r)
+            System.out.println("It Is A Valid Last Name");
+        else
+            System.out.println("It Is Invalid Last name");
+
+    }
+}
--- src/UserRegistrationMain.java
+++ src/UserRegistrationMain.java
@@ -1,21 +1,33 @@
+import java.util.Scanner;
 
-    import java.util.Scanner;
+public class UserRegistrationMain {
+    static Scanner scanner = new Scanner(System.in);
 
-    public class UserRegistrationMain {
-        static Scanner scanner = new Scanner(System.in);
+    public static void welcome() {
+        System.out.println("Welcome to User Registation Program");
+    }
 
-        public static void welcome() {
-            System.out.println("Welcome to User Registation Program");
-        }
+    public static void main(String[] args) {
+        welcome();
+        while (true) {
 
-        public static void main(String[] args) {
-            welcome();
-            System.out.println("Enter 1 : to validate First Name ");
+            System.out.println("Enter 0 : Exit");
+            System.out.println("Enter 1 : To validate First Name ");
+            System.out.println("Enter 2 : To validate Last Name ");
+
             switch (scanner.nextInt()) {
+                case 0:
+                    System.exit(0);
+
                 case 1:
                     UserRegistration.validFirstName();
                     break;
+
+                case 2:
+                    UserRegistration.validLastName();
+                    break;
+
             }
         }
     }
-
+}
