987075949389103b17a2e49145195601fe965295
nisha <nishantsingh187@gmail.com>
2022-12-12 02:28:52 +0530

    Email Sample

--- src/UserRegistration.java
+++ src/UserRegistration.java
@@ -95,7 +95,7 @@ public class UserRegistration {
     public static void validPassRule3() {
         System.out.println("Enter the Password Atleast One Upper Case & One Numaric");
         String passWord2 = scanner.nextLine();
-        String regex6 = "^[A-Z]{1}+[a-zA-Z]{6}+[0-9]+{1,}$";
+        String regex6 = "^[A-Z]{1}+[a-zA-Z]{6}+[0-9]{1,}$";
         Pattern p6 = Pattern.compile(regex6);
         Matcher m6 = p6.matcher(passWord2);
         boolean r6 = m6.matches();
